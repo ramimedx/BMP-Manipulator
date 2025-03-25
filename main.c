d0f25a8a0fecd71e1e87967e20372d5b171cfdde
ramimedx <59981560+ramimedx@users.noreply.github.com>
2025-03-25 16:23:51 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -16,13 +16,13 @@ ARGUMENTOS:
 "--invertir"
 "--concatenar-vertical nombre_de_la_nueva_imagen.bmp"       Ejemplo: --concatenar-vertical gato.bmp (puede concatenarse a si misma)
 "--concatenar-horizontal nombre_de_la_nueva_imagen.bmp"     Ejemplo: --concatenar-vertical gato.bmp (puede concatenarse a si misma)
-"--aumentar-contraste= numero entero"                       Ejemplo: --aumentar-contraste=50 (aumenta el contraste en un 50%)
-"--reducir-contraste= numero entero"
-"--tonalidad-azul= numero entero"
-"--tonalidad-verde= numero entero"
-"--tonalidad-roja= numero entero"
-"--recortar= numero entero"
-"--achicar= numero entero"
+"--aumentar-contraste= numero"                              Ejemplo: --aumentar-contraste=50 (aumenta el contraste en un 50%)
+"--reducir-contraste= numero"
+"--tonalidad-azul= numero"
+"--tonalidad-verde= numero"
+"--tonalidad-roja= numero"
+"--recortar= numero"
+"--achicar= numero"
 */
 
 int main(int argc, char* argv[])
